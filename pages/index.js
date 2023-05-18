3f91513c5116183cd2cf4580e09dd2f2376b467f
Madhumitha M <54738205+Madhu-29@users.noreply.github.com>
2023-05-18 05:33:44 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import styles from "../styles/homePage.module.css";
+
+export default function Caching({ time }) {
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>SSR cache testing</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>SSR cache testing</h1>
+        <p className={styles.card}>{time}</p>
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps({res}) {
+  res.setHeader("Cache-control","max-age=20")
+  return { props: {  time: new Date().toISOString() } };
+}
