99ae0e3ba9e4d31f695c3bcccc3eefd793af9c73
Madhumitha M <54738205+Madhu-29@users.noreply.github.com>
2023-05-18 05:57:45 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,6 @@ export default function Caching({ time }) {
 }
 
 export async function getServerSideProps({res}) {
-  res.setHeader("Cache-control","max-age=20")
+  res.setHeader("Cache-control","max-age=60")
   return { props: {  time: new Date().toISOString() } };
 }
